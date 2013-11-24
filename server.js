c03b4d44aea1d0f48839fd44e22476100e88ef6a
Elad Moshe <eladmoshe@gmail.com>
2013-11-24 07:13:46 +0000

    added path arguments

--- server.js
+++ server.js
@@ -5,28 +5,39 @@ var optimist = require('optimist'),
     logger = require('winston'),
     argv,
     app,
+    rootFolder,
+    hourMs
+;
 
-    rootFolder = path.join(__dirname, '../content');
 
 /*handle cli arguments*/
 argv = optimist
-    .usage('Usage: $0 --port [portNum]')
+    .usage('Usage: $0 --port [portNum] --folder [folderName]')
     .alias('p', 'port')
     .describe('p', 'Listen on port')
     .default('port', 8080)
+    .alias('f', 'folder')
+    .describe('f','Folder to serve')
+    .default('folder','/home/azureuser/Downloads/')
     .argv;
+
+hourMs = 1000*60*60;
+
+rootFolder = argv.folder;
 
 app = express()
     .use(express.favicon())
     .use(express.logger('dev'))
-    .use(express.bodyParser())
-    .use(express.static(rootFolder))
+    //.use(express.bodyParser())
+    .use(express.static(rootFolder, { maxAge: hourMs }))
     .use(express.directory(rootFolder))
+    .use(express.errorHandler());
 ;
 
 function run() {
     var server;
-    logger.info('- Mock server is running at port [', argv.port, ']');
+    logger.info('* Mock server is running at port [', argv.port, ']');
+    logger.info('* Serving folder [', rootFolder, ']'); 
     http.createServer(app).listen(argv.port);
 }
 
